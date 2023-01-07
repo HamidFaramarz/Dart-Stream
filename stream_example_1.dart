f7eb09a2f19b5a56785153e7aa0f5123e0256be3
Hamid Faramarz <101446076+HamidFaramarz@users.noreply.github.com>
2023-01-07 09:55:27 +0430

    Update stream_example_1.dart

--- stream_example_1.dart
+++ stream_example_1.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
 void main() {
+  
+  // for creating stream need to pass these steps: 
+  
   // create stream
   // access to stream
   // subscribe
+  // add data
 
   StreamController<int> controller = StreamController<int>();
   Stream stream = controller.stream;
